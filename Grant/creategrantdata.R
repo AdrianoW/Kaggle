0f7d9d559e409777e8bb83ebe1be0f5cac414a16
Adriano W Almeida <adrianowalmeida@gmail.com>
2014-09-02 16:58:59 +0200

    changes during the day

--- Grant/creategrantdata.R
+++ Grant/creategrantdata.R
@@ -64,6 +64,7 @@ raw$Contract.Value.Band...see.note.A <- factor(paste("ContractValueBand", raw$Co
 ## Change missing Role.1 information to Unk
 raw$Role.1 <- as.character(raw$Role.1)
 raw$Role.1[raw$Role.1 == ""] <- "Unk"
+raw$Start.date <- lapply(raw['Start.date'], as.Date, format='%d/%m/%y' )
 
 ## At this point, the data for investigators is in different
 ## columns. We'll take this "horizontal" format and convert it to a
@@ -192,165 +193,19 @@ noZV <- function(x)
 # Split
 #========================================================
 
-# shorten names
-names(investCount) <- shortNames(names(shortNames), "Num")
-# use the function
-tapply(vertical,  shortNames
+# add another column with the start dates into vertical
+dates <- rep(raw$Start.date[1], dim(vertical)[1])
+for (i in 1:dim(vertical)[1]){
+   dates[i] <- raw$Start.date[vertical$Grant.Application.ID[i]]
+}
+vertical2 <- vertical
+vertical2[, "Start.date"] <- dates
 
-######################################################################
-## Calculate the total number of people identified on the grant
-a =ddply(vertical, .(Grant.Application.ID), function (x) c(numPeople=nrow(x)))
+# find all the dates in year and after 2008
+ind_test <- which(unlist(lapply(vertical2['Start.date'], as.Date, format='%d/%m/%y')) >= as.Date('01/01/08', format = '%d/%m/%y'))
 
-######################################################################
-## Calculate the number of people per role
-# that is, a table with Grant.Application.ID NumCI NumDR NumECI NumEA NumHV NumPS NumSCI NumSR NumUNK ... etc
-library(reshape2)
-t <- dcast(vertical, Grant.Application.ID ~ Role, length  )
-View(t)
-######################################################################
-## For each role, calculate the frequency of people in each age group
-# that is, a table with Grant.Application.ID CI.1900 CI.1925 CI.1930 CI.1935 CI.1940 DR.1940 etc
-t <- dcast(vertical, Grant.Application.ID ~ Role+Year.of.Birth, length)
-View(t)
-######################################################################
-## For each role, calculate the frequency of people from each country
-# that is: names(investCountry)
-# [1] "Grant.Application.ID"   "CI.AsiaPacific"         "DR.AsiaPacific"
-# [4] "PS.AsiaPacific"         "CI.Australia"           "DR.Australia"
-# [7] "ECI.Australia"          "HV.Australia"           "PS.Australia", etc
-
-t <- dcast(vertical, Grant.Application.ID ~ Role+Country.of.Birth, length)
-View(t)
-
-######################################################################
-## For each role, calculate the frequency of people for each language
-# that is: names(investLang)
-# [1] "Grant.Application.ID" "CI.English"           "DR.English"
-# [4] "ECI.English"          "PS.English"           "CI.OtherLang"
-# [7] "DR.OtherLang"
-
-
-######################################################################
-## For each role, determine who as a Ph.D.
-# that is: > names(investPhD)
-# [1] "Grant.Application.ID" "CI.PhD"               "DR.PhD"
-# [4] "ECI.PhD"              "HV.PhD"               "PS.PhD"
-# [7] "SR.PhD"
-
-######################################################################
-## For each role, calculate the number of successful and unsuccessful
-## grants (**)
-# that is:  names(investGrants)
-# [1] "Grant.Application.ID" "Success.CI"           "Unsuccess.CI"
-# [4] "Success.DR"           "Unsuccess.DR"         "Success.ECI"
-# [7] "Unsuccess.ECI"        "Success.PS"           "Unsuccess.PS"
-# [10] "Success.HV"           "Unsuccess.HV"         "Success.SR"
-# [13] "Unsuccess.SR"
-
-melt(vertical[,c('Number.of.Unsuccessful.Grant','Number.of.Unsuccessful.Grant')])
-
-
-######################################################################
-## Create variables for each role/department combination
-# that is:  head(names(investDept))
-# [1] "Grant.Application.ID" "CI.Dept1013"          "CI.Dept1033"
-# [4] "DR.Dept1033"          "HV.Dept1033"          "PS.Dept1033"
-
-######################################################################
-## Create variables for each role/faculty #
-# head(names(investFaculty))
-# [1] "Grant.Application.ID" "CI.Faculty1"          "DR.Faculty1"
-# [4] "ECI.Faculty1"         "HV.Faculty1"          "PS.Faculty1" , etc
-
-
-######################################################################
-## Create dummy variables for each tenure length
-# head(names(investDuration))
-# [1] "Grant.Application.ID" "Duration0to5"         "Duration10to15"
-# [4] "Duration5to10"        "DurationGT15"         "DurationLT0"
-
-
-######################################################################
-## Create variables for the number of publications per journal
-## type. Note that we also compute the total number, which should be
-## removed for models that cannot deal with such a linear dependency
-
-
-######################################################################
-## Create variables for the number of publications per journal
-## type per role. (**)
-# names(investPub)
-# [1] "Grant.Application.ID" "Astar.CI"             "A.CI"
-# [4] "B.CI"                 "C.CI"                 "Astar.DR"
-# [7] "A.DR"                 "B.DR"                 "C.DR"
-# [10] "Astar.ECI"            "A.ECI"                "B.ECI"
-# [13] "C.ECI"                "Astar.PS"             "A.PS"
-# [16] "B.PS"                 "C.PS"                 "Astar.HV"
-# [19] "A.HV"                 "B.HV"                 "C.HV"
-# [22] "Astar.SR"             "A.SR"                 "B.SR"
-# [25] "C.SR"
-
-######################################################################
-## Create variables for each RFCD code
-# head(names(RFCDcount))
-# [1] "Grant.Application.ID" "RFCD210000"           "RFCD230101"
-# [4] "RFCD230102"           "RFCD230105"           "RFCD230107" etc
-
-######################################################################
-## Create variables for each SEO code
-# head(names(SEOcount))
-# [1] "Grant.Application.ID" "SEO610101"            "SEO610102"
-# [4] "SEO610103"            "SEO610104"            "SEO610199"
-# >
-
-
-######################################################################
-### Make dummy vars out of grant-specific data
-
-grantData <- raw[, c("Sponsor.Code", "Contract.Value.Band...see.note.A", "Grant.Category.Code")]
-
-## Make a lubridate object for the time, then derive the day, week and month info
-startTime <- dmy(raw$Start.date)
-
-grantData$Month <- factor(as.character(month(startTime, label = TRUE)))
-grantData$Weekday <- factor(as.character(wday(startTime, label = TRUE)))
-grantData$Day <- day(startTime)
-grantYear <- year(startTime)
-
-######################################################################
-### Use the dummyVars function to create binary variables for
-### grant-specific variables
-
-dummies <- dummyVars(~., data = grantData, levelsOnly = TRUE)
-grantData <- as.data.frame(predict(dummies, grantData))
-names(grantData) <- gsub(" ", "", names(grantData))
-
-grantData$Grant.Application.ID <- raw$Grant.Application.ID
-grantData$Class <- factor(ifelse(raw$Grant.Status, "successful", "unsuccessful"))
-grantData$Grant.Application.ID <- raw$Grant.Application.ID
-
-grantData$is2008 <- year(startTime) == 2008
-grantData <- noZV(grantData)
-
-######################################################################
-### Merge all the predictors together, remove zero variance columns
-### and merge in the outcome data
-summarized <- merge(investCount, investDOB)
-summarized <- merge(summarized, investCountry)
-summarized <- merge(summarized, investLang)
-summarized <- merge(summarized, investPhD)
-summarized <- merge(summarized, investGrants)
-summarized <- merge(summarized, investDept)
-summarized <- merge(summarized, investFaculty)
-summarized <- merge(summarized, investDuration)
-summarized <- merge(summarized, investPub)
-summarized <- merge(summarized, totalPub)
-summarized <- merge(summarized, people)
-summarized <- merge(summarized, RFCDcount)
-summarized <- merge(summarized, SEOcount)
-summarized <- merge(summarized, grantData)
-## Remove the ID column
-summarized$Grant.Application.ID <- NULL
-print(str(summarized))
+#split up into test and train
+train <- vertical2[-ind_test,]
+test <- vertical2[ind_test,]
 
 
